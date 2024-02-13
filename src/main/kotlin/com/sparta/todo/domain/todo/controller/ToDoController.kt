d7ebc2d3d476b9eb0bcb016361d7bf4d6644dd89
distecter <distecter@naver.com>
2024-02-13 14:42:02 +0900

    4

--- src/main/kotlin/com/sparta/todo/domain/todo/controller/ToDoController.kt
+++ src/main/kotlin/com/sparta/todo/domain/todo/controller/ToDoController.kt
@@ -3,6 +3,8 @@ package com.sparta.todo.domain.todo.controller
 import com.sparta.todo.domain.todo.dto.CreateToDoRequest
 import com.sparta.todo.domain.todo.dto.ToDoResponse
 import com.sparta.todo.domain.todo.dto.UpdateToDoRequest
+import com.sparta.todo.domain.todo.service.ToDoService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
@@ -15,26 +17,35 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/ToDo")
-class ToDoController {
+class ToDoController(private val toDoService: ToDoService) {
 
     @GetMapping("/{todoId}")
-    fun getTodo(@PathVariable todoId: Long) {
-        TODO()
+    fun getTodo(@PathVariable todoId: Long): ResponseEntity<ToDoResponse> {
+        val toDoResponse = toDoService.getToDoById(todoId)
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(toDoResponse)
     }
 
     @PostMapping
-    fun createToDo(@RequestBody createToDoRequest: CreateToDoRequest):
-            ResponseEntity<ToDoResponse> {
-        TODO()
+    fun createToDo(@RequestBody createToDoRequest: CreateToDoRequest
+    ): ResponseEntity<ToDoResponse> {
+        val createdToDo = toDoService.createToDo(createToDoRequest)
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(createdToDo)
     }
 
     @PutMapping("/{todoId}")
-    fun updateToDo(@PathVariable todoId: Long, @RequestBody updateToDoRequest: UpdateToDoRequest){
-        TODO()
+    fun updateToDo(@PathVariable todoId: Long, @RequestBody updateToDoRequest: UpdateToDoRequest
+    ): ResponseEntity<Unit> {
+        toDoService.updateToDo(todoId, updateToDoRequest)
+        return ResponseEntity.status(HttpStatus.OK).build()
     }
 
     @DeleteMapping("/{todoId}")
-    fun deleteToDo(@PathVariable todoId: Long) {
-        TODO()
+    fun deleteToDo(@PathVariable todoId: Long): ResponseEntity<Unit> {
+        toDoService.deleteToDo(todoId)
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build()
     }
 }
